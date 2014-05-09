d311ff63a4500e279a207cee3cebbb0224362491
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-08 19:24:54 -0500

    autopush

--- drawing.h
+++ drawing.h
@@ -1,9 +1,8 @@
 
 
 void pp(int x, int y, int intensity, int c) {
-    // calculate the pixel's byte offset inside the buffer
     unsigned int pix_offset = x + y * finfo.line_length;
-    
-    // now this is about the same as 'fbp[pix_offset] = value'
-    *((char*)(fbp + pix_offset)) = c;
+    int g = c & 0xFF;
+    g = g / (double) 255 * intensity;
+    *((char*)(fbp + pix_offset)) = ((g&0x0ff)<<16)|((g&0x0ff)<<8)|(g&0x0ff);;
 }
