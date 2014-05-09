e39c5ee35d4768fbbb99d1f2b76353daab43e2da
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-09 18:20:58 -0500

    autopush

--- drawing.h
+++ drawing.h
@@ -1,11 +1,11 @@
 
 
 void pp(int x, int y, int intensity, int c) {
-    wprintf("%d", c);
+    wprintf(L"%d", c);
     unsigned int pix_offset = x + y * finfo.line_length;
     int r = ((c & 0xFF) << 16);
 	int g = ((c & 0xFF) << 8);
    	int b = (c & 0xFF);
-   	wprintf("%d %d %d", r, g, b);
+   	wprintf(L"%d %d %d", r, g, b);
     *((char*)(fbp + pix_offset)) = ((r&0x0ff)<<16)|((g&0x0ff)<<8)|(b&0x0ff);
 }
