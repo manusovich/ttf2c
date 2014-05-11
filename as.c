5c205e5ecc4dd285aa1fc1e2c0b33594b5bbb76a
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-11 14:18:31 -0500

    autopush

--- as.c
+++ as.c
@@ -9,6 +9,14 @@
 #include <wchar.h> 
 #include <wctype.h> 
 #include <locale.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
 
 // 'global' variables to store screen info
 char *fbp = 0;
@@ -100,8 +108,45 @@ int main(int argc, char* argv[])
         wprintf(L"Failed to mmap.\n");
     }
     else {
+        int sock,bytes_recv,sin_size;
+        struct sockaddr_in server_addr;
+        struct hostent *host;
+        char send_data[1024],recv_data[1024];
+
+        host= (struct hostent *) gethostbyname((char *)"192.168.105.81");
+
+        if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+        {
+            perror("socket");
+            exit(1);
+        }
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(8088);
+        server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+        bzero(&(server_addr.sin_zero),8);
+        sin_size = sizeof(struct sockaddr);
+
+        while (1)
+           {
+
+            printf("Type Something (q or Q to quit):");
+            gets(send_data);
+
+            if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0)
+               break;
+
+            else
+               sendto(sock, send_data, strlen(send_data), 0,
+                      (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+
+                 bytes_recv = recvfrom(sock,recv_data,1024,0,(struct sockaddr *)&server_addr,&sin_size);
+                  recv_data[bytes_recv]= '\0';
+             printf("Received :%s\n",recv_data);
+        }
+
         // draw...
-        draw();
+//        draw();
     }
     
     // cleanup
