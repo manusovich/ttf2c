763aea39418b78e3f3df1cb4de2547f5e3260a83
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-11 14:39:09 -0500

    autopush

--- client.c
+++ client.c
@@ -10,7 +10,7 @@
 
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to 
+#define PORT "8088" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
 
