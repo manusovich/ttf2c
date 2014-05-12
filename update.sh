870e3cc1e4bca95d7efceb6d3b40a662d4c3f75e
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-12 11:07:59 -0500

    autopush

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-git fetch
-
+env GIT_SSL_NO_VERIFY=true git fetch
 
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
@@ -11,6 +10,7 @@ if [ $LOCAL = $REMOTE ]; then
 echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
 echo "Need to pull"
+git rebase origin/master 
 elif [ $REMOTE = $BASE ]; then
 echo "Need to push"
 else
