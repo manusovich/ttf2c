b595ab6385a8842a7f2b07db66c9d9fded292a30
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-12 13:12:08 -0500

    autopush

--- as.c
+++ as.c
@@ -1,6 +1,3 @@
-#define MAIN_FILE
-
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,15 +18,72 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
-#define PORT "8088" // the port client will be connecting to 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
-#include "drawing.h"
 #include "fl.h"
 #include "fs.h"
 
+#define PORT "8088" // the port client will be connecting to 
+#define MAXDATASIZE 100 // max number of bytes we can get at once 
 #define BUF_SIZE 100
 wchar_t wcsbuf[BUF_SIZE];
+
+char *fbp = 0;
+struct fb_var_screeninfo vinfo;
+struct fb_fix_screeninfo finfo;
+
+void put_pixel(int x, int y, int c) {
+    unsigned int pix_offset = x + y * finfo.line_length;
+    *((char*)(fbp + pix_offset)) = c;
+}
+
+int rgb(int r, int g, int b) {
+    return ((r & 0x0ff) << 16) | ((g & 0x0ff)<<8) | (b & 0x0ff);
+}
+
+// helper function to clear the screen - fill whole 
+// screen with given color
+void clear_screen(int c) {
+    memset(fbp, c, vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8);
+}
+
+void put_pixel_RGB565(int x, int y, int r, int g, int b)
+{
+
+    // calculate the pixel's byte offset inside the buffer
+    // note: x * 2 as every pixel is 2 consecutive bytes
+    unsigned int pix_offset = x * 2 + y * finfo.line_length;
+
+    // now this is about the same as 'fbp[pix_offset] = value'
+    // but a bit more complicated for RGB565
+    unsigned short c = ((r / 8) << 11) + ((g / 4) << 5) + (b / 8);
+    // or: c = ((r / 8) * 2048) + ((g / 4) * 32) + (b / 8);
+    // write 'two bytes at once'
+    *((unsigned short*)(fbp + pix_offset)) = c;
+}
+
+void put_pixel_RGB24(int x, int y, int r, int g, int b)
+{
+    // calculate the pixel's byte offset inside the buffer
+    // note: x * 3 as every pixel is 3 consecutive bytes
+    unsigned int pix_offset = x * 3 + y * finfo.line_length;
+
+    // now this is about the same as 'fbp[pix_offset] = value'
+    *((char*)(fbp + pix_offset)) = r;
+    *((char*)(fbp + pix_offset + 1)) = g;
+    *((char*)(fbp + pix_offset + 2)) = b;
+}
+
+void pp(int x, int y, int intensity, int c) {
+    unsigned int pix_offset = x + y * finfo.line_length;
+    int r = ((c >> 16) & 0x0FF) / (double) 0xff * intensity;
+    int g = ((c >> 8) & 0x0FF) / (double) 0xff * intensity;
+    int b = (c & 0x0FF) / (double) 0xff * intensity;
+
+    if (vinfo.bits_per_pixel == 16) {
+        put_pixel_RGB565(x, y, r, g, b);
+    } else {
+        put_pixel_RGB24(x, y, r, g, b);
+    }
+}
 
 void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
