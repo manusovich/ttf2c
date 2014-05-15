8177af253f2708200eb0e33f692adc1029b51e91
Alex <amanusovich1@MZD-AManusovich.local>
2014-05-14 19:06:02 -0500

    autopush

--- s.c
+++ s.c
@@ -1,4 +1,23 @@
-#include   "unp.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <wchar.h> 
+#include <wctype.h> 
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main(int argc, char **argv) {
 	int					sockfd, n;
